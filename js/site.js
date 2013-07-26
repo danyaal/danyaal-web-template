b0068e28726295c113b530272d35d512e6751689
Danyaal Rangwala <thedanyaal@gmail.com>
2013-07-26 02:37:57 -0700

    Beginning new categories for site

--- js/site.js
+++ js/site.js
@@ -10,26 +10,12 @@ $(document).ready(function() {
 	$('body').hide();
 	$('#back').hide();
 	$('body').fadeIn(2000);
-	// DESIGNER
-	$('.designer-element').hide();
-	$('#designer').click(function() {
+	// PORTFOLIO
+	$('.portfolio-element').hide();
+	$('#portfolio').click(function() {
 		$('.main').fadeOut(0);
 		$('#back').fadeIn(500);
-		$('.designer-element').fadeIn(500);
-	});
-	// DEVELOPER
-	$('.developer-element').hide();
-	$('#developer').click(function() {
-		$('.main').fadeOut(0);
-		$('#back').fadeIn(500);
-		$('.developer-element').fadeIn(500);
-	});
-	// LEADER
-	$('.leader-element').hide();
-	$('#leader').click(function() {
-		$('.main').fadeOut(0);
-		$('#back').fadeIn(500);
-		$('.leader-element').fadeIn(500);
+		$('.portfolio-element').fadeIn(500);
 	});
 	// CONTACT
 	$('.contact-element').hide();
@@ -43,9 +29,7 @@ $(document).ready(function() {
 	$('#back').click(function() {
 		$('#back').fadeOut(0);
 		$('.main').fadeIn(500);
-		$('.designer-element').fadeOut(0);
-		$('.developer-element').fadeOut(0);
-		$('.leader-element').fadeOut(0);
+		$('.portfolio-element').fadeOut(0);
 		$('.contact-element').fadeOut(0);
 	});
 	
