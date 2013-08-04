1c9abc840cc5ccc9d5989005908a98c95b6a88f7
Danyaal Rangwala <thedanyaal@gmail.com>
2013-08-04 01:54:47 -0700

    Refactor code, add story, style changes

--- js/site.js
+++ js/site.js
@@ -1,36 +1,39 @@
-// For every category added to the home page, create a main category here that:
-// 		1. Starts its sub-elements (if any) off as hidden
-// 		2. On click, fadeOut(0) ".main" and fadeIn(500) "#back" and fadeIn(500) its sub-elements
-// 		3. On "#back" click, fadeOut(0) its sub-elements
-// I promise to refactor this so that it's much less code later.
+// I have laid out the js so that when you want to add a new category, you just need to:
+// 		1. Make sure if it has sub-elements, that you need to tag them with the 'sub-element' class in index.html
+// 		2. And that on click, you fadeOut(0) '.main' and fadeIn(500) '#back' and fadeIn(500) the sub-elements
+// I promise to refactor this so that it's much less code (and work for you) later.
 // <3 - Danyaal
 
 $(document).ready(function() {
 	// ENTER SITE
 	$('body').hide();
 	$('#back').hide();
+	$('.sub-element').hide();
 	$('body').fadeIn(2000);
+	// ABOUT
+	$('#about').click(function() {
+		$('.main').fadeOut(0);
+		$('#back').fadeIn(500);
+		$('#about-element').fadeIn(500);
+	});
 	// PORTFOLIO
-	$('.portfolio-element').hide();
 	$('#portfolio').click(function() {
 		$('.main').fadeOut(0);
 		$('#back').fadeIn(500);
-		$('.portfolio-element').fadeIn(500);
+		$('#portfolio-element').fadeIn(500);
 	});
 	// CONTACT
-	$('.contact-element').hide();
 	$('#contact').click(function() {
 		$('.main').fadeOut(0);
 		$('#back').fadeIn(500);
-		$('.contact-element').fadeIn(500);
-		// $('.contact-element').fadeToggle(500);
+		$('#contact-element').fadeIn(500);
+		// $('#contact-element').fadeToggle(500);
 	});
 	// BACK
 	$('#back').click(function() {
 		$('#back').fadeOut(0);
 		$('.main').fadeIn(500);
-		$('.portfolio-element').fadeOut(0);
-		$('.contact-element').fadeOut(0);
+		$('.sub-element').fadeOut(0);
 	});
 	
 	// FANCYBOX
